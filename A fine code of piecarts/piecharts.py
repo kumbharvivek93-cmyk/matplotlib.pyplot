c58644c27ec079b3c6531ce49cb45a9f9d4fa1fb
Vivek Kumbhar <kumbharvivek93@gmail.com>
2026-02-01 18:10:02 +0530

    Update piecharts.py

--- A fine code of piecarts/piecharts.py
+++ A fine code of piecarts/piecharts.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 labels=["vivek","ranjana","dada","teju"]
 income=[2400000,1700000,1500000,30000]  # always set legend for the pie charts
 colours=["blue","yellow","green","grey"]
@@ -7,3 +9,4 @@ plt.pie(income,labels=labels,explode=[0,0,0,0],shadow=True,startangle=100,colors
 plt.legend()
 plt.grid(True)
 plt.show()
+# use the legend() always while doing with the piecharts and also look for expode list and colours list and startangle and shadows settings if needed
